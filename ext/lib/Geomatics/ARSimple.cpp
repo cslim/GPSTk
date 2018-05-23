f865c8e62d496542f9ab47c9d03c6226d28a1996
Unknown <vm.dmitriev90@gmail.com>
2018-05-23 10:52:13 +0300

    Added classes for integer ambiguities resolution

---
+++ ext/lib/Geomatics/ARSimple.cpp
@@ -0,0 +1,43 @@
+
+#include "ARSimple.hpp"
+
+namespace gpstk
+{
+   Vector<double> ARSimple::resolveIntegerAmbiguity( 
+                                                 const Vector<double>& ambFloat, 
+                                                 const Matrix<double>& ambCov )
+      throw(ARException)
+   {
+         // check input
+      if( ambFloat.size()!=ambCov.rows() || ambFloat.size()!=ambCov.cols() )
+      {
+         ARException e("The dimension of input does not match.");
+         GPSTK_THROW(e);
+      }
+
+      const size_t n = ambFloat.size();
+
+      Vector<double> ambFixed(n,0.0);
+      for(int i = 0; i < ambFloat.size(); i++)
+      {
+         const double threshold = 3.0 * std::sqrt(ambCov(i,i));
+
+         double lowerValue = ambFloat(i) - threshold;
+         double upperValue = ambFloat(i) + threshold;
+
+         if( std::abs(upperValue-lowerValue) <= 1.0 )
+         {
+            ambFixed(i) = double( std::floor(ambFloat(i)+0.5) );
+         }
+         else
+         {
+            ambFixed(i) = ambFloat(i);       // keep it with float value
+         }
+      }
+
+      return ambFixed;
+
+   }  // End of method 'ARSimple::resolveIntegerAmbiguity()'
+
+   
+}   // End of namespace gpstk
